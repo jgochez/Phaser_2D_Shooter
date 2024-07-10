67eaf16b89659cb73e4fb2fc200f9c4caa1bfc55
Liam K. Pestrella <114099477+liamp55@users.noreply.github.com>
2024-07-09 17:29:17 -0700

    Update main.js
    added code for frontend functionality

--- static/main.js
+++ static/main.js
@@ -1,13 +1,14 @@
+
 var config = {
     type: Phaser.AUTO,
     width: 800,
-    height: 600,
-    backgroundColor: '#000000',
+    height: 700,
     physics: {
         default: 'arcade',
         arcade: {
+            gravity: {y:30},
             debug: false
-        }
+        },
     },
     scene: {
         preload: preload,
@@ -16,16 +17,71 @@ var config = {
     }
 };
 
+
+
 var game = new Phaser.Game(config);
 
-function preload () {
-    // Game assets
+
+function preload ()
+{
+    // loading images
+    this.load.image('space', 'images/background1.jpeg');
+    this.load.image('ship', 'images/ship1.png');
+    this.load.image('big-asteroid', 'images/asteroids-sprite.png');
+
 }
 
-function create () {
-    // Game objects 
+function create ()
+{
+
+    // moving space background
+    this.tileSprite = this.add.tileSprite(0, 0, innerWidth, innerHeight, 'space');
+    this.tileSprite.setOrigin(0);
+    this.tileSprite.setScrollFactor(0, 1);
+
+    // asteroid handling which depends on level
+
+    this.add.image(400, 100, 'big-asteroid');
+    this.physics.add.sprite(400, 100, 'big-asteroid');
+
+
+    // main-ship logic
+    main_ship = this.physics.add.sprite(400, 630, 'ship');
+    main_ship.setScale(.2);
+    main_ship.setCollideWorldBounds(true);
+   
+
+
+    
+
 }
 
-function update () {
-    // Game logic 
+function update ()
+{
+    // moving background logic
+    this.tileSprite.tilePositionY -= 5;
+
+    // moving ship with keys
+    cursors = this.input.keyboard.createCursorKeys();
+
+    if (cursors.left.isDown) {
+        main_ship.setVelocityX(-200);
+        main_ship.anims.play('left', true);
+    }
+
+    else if (cursors.right.isDown) {
+        main_ship.setVelocityX(200);
+        main_ship.anims.play('right', true);
+    }
+
+    if (cursors.up.isDown) {
+        main_ship.setVelocityY(-200);
+        main_ship.anims.play( 'up', true);
+    }
+    else if (cursors.down.isDown) {
+        main_ship.setVelocityY(200);
+        main_ship.anims.play('down', true);
+    }
+    
+
 }
